c06f1b0aa11d19206ab7b4f8d2f60ca3bfa633d9
Thomas Maurer <ixolidiazerty@gmail.com>
2019-08-21 16:57:35 +0200

    added in index everything carousel

--- index.php
+++ index.php
@@ -3,47 +3,58 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<link href="css/header.css" rel="stylesheet" type="text/css" />
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href="css/header.css" rel="stylesheet" type="text/css" />
     <link href="css/jokebox.css" rel="stylesheet" type="text/css" />
-	<meta charset="utf-8">
+
+    <link href="css/carousel/carousel.css" rel="stylesheet" type="text/css" />
+
+    <link rel="stylesheet" href="/css/carousel/slick.css">
+    <link rel="stylesheet" href="/css/carousel/slick-theme.css">
+    <meta charset="utf-8">
 </head>
+
 <body>
 
 
 
-	
 
-	<div class="grid-container">
 
-		<div class="header">
-			<?php include "php/header.php"?>
-		</div>
-		  
-		<div class="main">
-			
-			<div class="jokebox">
-				<?php include "php/jokeBox.php"?>
-			</div>
-			
-			<div class="genre">
-				<?php include "php/carousel.php"?>
-			</div>
-			
-			<div class="listsong">
-				<?php include "php/listsong.php"?>
-			</div>
-			
-  		</div>
-  		
-	</div>
+    <div class="grid-container">
 
-	<script src="js/header.js"></script>
-	<script src="js/jokeBox.js"></script>
-	
+        <div class="header">
+            <?php include "php/header.php" ?>
+        </div>
 
-	<script src="js/main.js"></script>
+        <div class="main">
+
+            <div class="jokebox">
+                <?php include "php/jokeBox.php" ?>
+            </div>
+
+            <div class="genre">
+                <?php include "php/carousel.php" ?>
+            </div>
+
+            <div class="listsong">
+                <?php include "php/listsong.php" ?>
+            </div>
+
+        </div>
+
+    </div>
+
+    <script src="js/header.js"></script>
+    <script src="js/jokeBox.js"></script>
+
+
+    <script src="js/main.js"></script>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/slick.js"></script>
 </body>
+
 </html>
