9c3a9e3b2e8d6f353911dad3d14db0c0cd695fc8
SalasDorian <salasdorian@live.fr>
2019-08-21 16:45:01 +0200

    import and other

--- index.php
+++ index.php
@@ -6,12 +6,19 @@ session_start();
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href="css/header.css" rel="stylesheet" type="text/css" />
+    <link href="css/jokebox.css" rel="stylesheet" type="text/css" />
 	<meta charset="utf-8">
 </head>
 <body>
 
 
-	<?php  ?>
+	<?php include('html/header.php') ?>
+	<?php include('html/jokebox.php') ?>
+	
+	
+	<script src="js/jokeBox.js"></script>
+	<script src="js/header.js"></script>
 	<script src="js/main.js"></script>
 </body>
 </html>
