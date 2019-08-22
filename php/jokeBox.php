3723395b5bb88d0e1baddfc5d518b5b316fbaebd
Thomas Maurer <ixolidiazerty@gmail.com>
2019-08-22 12:31:42 +0200

    Added infos

--- php/jokeBox.php
+++ php/jokeBox.php
@@ -1,88 +1,24 @@
 <div class="jukebox">
-  <div class="jukebox__head">
-    <div class="jukebox__record">
-      <img class="jukebox__record-image" src="https://i1.sndcdn.com/artworks-000218580276-2mqw0f-t500x500.jpg" />
+    <div class="jukebox__head">
+        <div class="jukebox__record">
+            <img class="jukebox__record-image" src="https://i1.sndcdn.com/artworks-000218580276-2mqw0f-t500x500.jpg" />
+        </div>
+        <!--     <div class="jukebox__rating"></div> -->
     </div>
-<!--     <div class="jukebox__rating"></div> -->
-  </div>
-  <div class="jukebox__body">
-    <div class="jukebox__internal">
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+    <div class="jukebox__body">
+        <div class="jukebox__internal">
+            <ul>
+                <?php
+                for ($i = 0; $i < 71; $i++) {
+                    echo '<li></li>';
+                }
+                ?>
+            </ul>
+        </div>
     </div>
-  </div>
-  <div class="jukebox__leg jukebox__leg--left"></div>
-  <div class="jukebox__leg jukebox__leg--right"></div>
+    <div class="jukebox__leg jukebox__leg--left"></div>
+    <div class="jukebox__leg jukebox__leg--right"></div>
+</div>
+<div id="jukeInfos">
+    <h3>INFOS</h3>
 </div>
