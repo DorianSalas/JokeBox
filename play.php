1e4c15ac6b157480b540619b4bf5b9efe8b25640
NightenDushi <nathan.guilhot@gmx.fr>
2019-08-22 16:11:56 +0200

    tracklist is working better

--- play.php
+++ play.php
@@ -26,6 +26,21 @@
 		<script type="text/javascript">
 			let listmusicreal
 			let btn_music
+			let playerdiv=document.getElementById("audio_player");
+			let player=document.getElementById("player");
+
+			let endfunc=function(){
+				console.log(player.attributes.track.value)
+				if (player.attributes.track.value<listmusicreal.length-1) {
+					playerdiv.innerHTML="<audio id='player' controls track='"+(player.attributes.track.value++)+"'><source src='music/"+listmusicreal[player.attributes.track.value++]+"' /></audio>"
+					player=document.getElementById("player");
+					player.onended=endfunc;
+					console.log(player.attributes.track.value++)
+				}
+				else{
+					console.log("Fin de la liste")
+				}
+			}
 
 			function getList(){
 				btn_music=document.getElementsByClassName("song");
@@ -35,24 +50,12 @@
 					listmusicreal.push(btn_music[i].attributes.value.value)
 				}
 				console.log(listmusicreal)
+
+				playerdiv.innerHTML="<audio id='player' controls autoplay track='0'><source src='music/"+listmusicreal[0]+"' /></audio>";
+				player=document.getElementById("player");
+				player.onended=endfunc;
 			}
 			getList()
-
-			let playerdiv=document.getElementById("audio_player");
-			let player=document.getElementById("player");
-
-			let endfunc=function(){
-				console.log(player.attributes.track.value)
-				if (player.attributes.track.value<listmusicreal.length) {
-					playerdiv.innerHTML="<audio id='player' controls autoplay track='"+(player.attributes.track.value++)+"'><source src='music/"+listmusicreal[player.attributes.track.value++]+"' /></audio>"
-					player=document.getElementById("player");
-					player.onended=endfunc;
-					console.log(player.attributes.track.value++)
-				}
-				else{
-					console.log("Fin de la liste")
-				}
-			}
 
 			for (let i = 0; i < listmusicreal.length; i++) {
 				console.log(listmusicreal[i])
