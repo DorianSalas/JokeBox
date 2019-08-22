d91b204f0050983f08e85d8fe8679306600c96ec
SalasDorian <salasdorian@live.fr>
2019-08-22 10:37:04 +0200

    header good but not

--- php/header.php
+++ php/header.php
@@ -1,1 +1,18 @@
 <!-- bla -->
+<header>
+
+    <div id='divCo'>
+        <p>Log In</p>
+        <div class="arrow-left"></div>
+    </div>
+
+    <div id='divTitle'>
+        <h1>JokeBox</h1>
+    </div>
+
+    <div id='divDeco'>
+        <div class="arrow-right"></div>
+        <p>Log Out</p>
+    </div>
+
+</header>
