7080bcbe45b1c39bd4c2e9754a2ee2f97e5abd11
SalasDorian <salasdorian@live.fr>
2019-08-22 11:44:57 +0200

    debut jokebox

--- php/jokeBox.php
+++ php/jokeBox.php
@@ -1,1 +1,88 @@
-<!-- bla -->
+<div class="jukebox">
+  <div class="jukebox__head">
+    <div class="jukebox__record">
+      <img class="jukebox__record-image" src="https://i1.sndcdn.com/artworks-000218580276-2mqw0f-t500x500.jpg" />
+    </div>
+<!--     <div class="jukebox__rating"></div> -->
+  </div>
+  <div class="jukebox__body">
+    <div class="jukebox__internal">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+  <div class="jukebox__leg jukebox__leg--left"></div>
+  <div class="jukebox__leg jukebox__leg--right"></div>
+</div>
