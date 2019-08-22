fe471275075309822ca846f1ba36170ac001d489
NightenDushi <nathan.guilhot@gmx.fr>
2019-08-22 10:49:47 +0200

    Progress on listsong

--- php/listsong.php
+++ php/listsong.php
@@ -3,12 +3,33 @@
 <div id="listsong_main"> <!-- main -->
 	<div id="filtertab">
 		<!-- filter -->
-		FILTERS
+		<div id="filter_boxes">
+			<div class="filter_box">
+			  <input type="radio" id="az_order" name="filter" value="az_order">
+			  <label for="az_order">A-Z</label>
+			</div>
+			<div class="filter_box">
+			  <input type="radio" id="za_order" name="filter" value="za_order">
+			  <label for="za_order">Z-A</label>
+			</div>
+			<div class="filter_box">
+			  <input type="radio" id="artist" name="filter" value="artist">
+			  <label for="artist">Artist</label>
+			</div>
+			<div class="filter_box">
+			  <input type="radio" id="date" name="filter" value="date">
+			  <label for="date">Date</label>
+			</div>
+		</div>
+		<div id="filter_input">
+			<input type="text" name="search">
+		</div>
+
 	</div>
 	<div id="listsong">
 		<!-- listsong -->
-		<div class="song">
-			<img src="img/Nani.png" class="song_img">
+		<div class="song play">
+			<div src="img/Nani.png" class="song_img"></div>
 			<div class="song_info">
 				<div class="song_title">Sandstorm</div> <!-- Titre -->
 				<div class="song_artist">Darude</div> <!-- Auteur -->
@@ -16,7 +37,7 @@
 		</div>
 
 		<div class="song">
-			<img src="img/Nani.png" class="song_img">
+			<div src="img/Nani.png" class="song_img"></div>
 			<div class="song_info">
 				<div class="song_title">No Halo</div> <!-- Titre -->
 				<div class="song_artist">BROCKHAMPTON</div> <!-- Auteur -->
@@ -25,50 +46,41 @@
 
 		<!-- TODELETE -->
 		<div class="song">
-			<img src="img/Nani.png" class="song_img">
+			<div src="img/Nani.png" class="song_img"></div>
 			<div class="song_info">
-				<div class="song_title">Sandstorm</div> <!-- Titre -->
-				<div class="song_artist">Darude</div> <!-- Auteur -->
-			</div>			
-		</div>
-
-		<div class="song">
-			<img src="img/Nani.png" class="song_img">
-			<div class="song_info">
-				<div class="song_title">No Halo</div> <!-- Titre -->
-				<div class="song_artist">BROCKHAMPTON</div> <!-- Auteur -->
+				<div class="song_title">FCKD</div> <!-- Titre -->
+				<div class="song_artist">Hyper</div> <!-- Auteur -->
 			</div>			
 		</div>
 		<div class="song">
-			<img src="img/Nani.png" class="song_img">
+			<div src="img/Nani.png" class="song_img"></div>
 			<div class="song_info">
-				<div class="song_title">Sandstorm</div> <!-- Titre -->
-				<div class="song_artist">Darude</div> <!-- Auteur -->
-			</div>			
-		</div>
-
-		<div class="song">
-			<img src="img/Nani.png" class="song_img">
-			<div class="song_info">
-				<div class="song_title">No Halo</div> <!-- Titre -->
-				<div class="song_artist">BROCKHAMPTON</div> <!-- Auteur -->
+				<div class="song_title">The Resistance</div> <!-- Titre -->
+				<div class="song_artist">Drivepilot</div> <!-- Auteur -->
 			</div>			
 		</div>
 		<div class="song">
-			<img src="img/Nani.png" class="song_img">
+			<div src="img/Nani.png" class="song_img"></div>
 			<div class="song_info">
-				<div class="song_title">Sandstorm</div> <!-- Titre -->
-				<div class="song_artist">Darude</div> <!-- Auteur -->
+				<div class="song_title">Desolation</div> <!-- Titre -->
+				<div class="song_artist">Slooze</div> <!-- Auteur -->
 			</div>			
 		</div>
-
 		<div class="song">
-			<img src="img/Nani.png" class="song_img">
+			<div src="img/Nani.png" class="song_img"></div>
 			<div class="song_info">
-				<div class="song_title">No Halo</div> <!-- Titre -->
-				<div class="song_artist">BROCKHAMPTON</div> <!-- Auteur -->
+				<div class="song_title">Miami Showdown</div> <!-- Titre -->
+				<div class="song_artist">Digitalism</div> <!-- Auteur -->
 			</div>			
 		</div>
+		<div class="song">
+			<div src="img/Nani.png" class="song_img"></div>
+			<div class="song_info">
+				<div class="song_title">Omega</div> <!-- Titre -->
+				<div class="song_artist">Absolute Valentine</div> <!-- Auteur -->
+			</div>			
+		</div>
+		
 
 	</div>
 </div>
