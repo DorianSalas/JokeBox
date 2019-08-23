6524103193cd53885bad6497df48af636161eb22
SalasDorian <salasdorian@live.fr>
2019-08-23 15:41:02 +0200

    logo simplon anime

--- php/jokeBox.php
+++ php/jokeBox.php
@@ -12,13 +12,24 @@
     </div>
     <div class="jukebox__body">
         <div class="jukebox__internal">
-            <ul>
+            <!-- <ul>
+
+                <img src='img/simplon.png'>
                 <?php
-                for ($i = 0; $i < 71; $i++) {
-                    echo '<li></li>';
-                }
+                // for ($i = 0; $i < 71; $i++) {
+                //     echo '<li></li>';
+                // }
                 ?>
-            </ul>
+            </ul> -->
+            <div class="logo-flip-box" data-toggle="modal" data-target="#exampleModalCenter">
+                <div id="logo-front-side">
+                    <img class="logo-front-side" src="img/simplon.png" alt="">
+                </div>
+                <div id="logo-back-side">
+                    <img class="logo-back-side" src="img/simpsonne.png" alt="">
+                </div>
+            </div>
+
         </div>
     </div>
     <div class="jukebox__leg jukebox__leg--left"></div>
@@ -26,12 +37,12 @@
 </div>
 <div id="jukeInfos">
     <h3>INFOS</h3>
-    <svg width="200px" height="100%" viewBox="0 0 42 42" class="donut">
+    <!-- <svg width="200px" height="100%" viewBox="0 0 42 42" class="donut">
   <circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="transparent">teoihouhst</circle>lokn
   <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#459AC2" stroke-width="10"></circle>
   <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#BC3135" stroke-width="10" stroke-dasharray="47.5 60" stroke-dashoffset="450">blablablablalbla</circle>
   <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#418452" stroke-width="10" stroke-dasharray="15 85" stroke-dashoffset="0"></circle>
   <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#505085" stroke-width="10" stroke-dasharray="32.5 67.5" stroke-dashoffset="-15"></circle>
-</svg>
+</svg> -->
 
 </div>
