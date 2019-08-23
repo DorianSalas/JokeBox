49c155a9c72d24ea0a7af94bbaa26d70b01efbba
SalasDorian <salasdorian@live.fr>
2019-08-23 16:41:15 +0200

    change img disc

--- js/jokeBox.js
+++ js/jokeBox.js
@@ -1,67 +1,72 @@
 let listmusicreal
 let btn_music
-let playerdiv=document.getElementById("audio_player");
-let player=document.getElementById("player");
+let playerdiv = document.getElementById("audio_player");
+let player = document.getElementById("player");
 
-let endfunc=function(){
-	console.log(player.attributes.track.value)
-	if (player.attributes.track.value<listmusicreal.length-1) {
-		playerdiv.innerHTML="<audio id='player' controls track='"+(player.attributes.track.value++)+"'><source src='music/"+listmusicreal[player.attributes.track.value++]+"' /></audio>"
-		player=document.getElementById("player");
-		player.onended=endfunc;
-		console.log(player.attributes.track.value++)
+let endfunc = function() {
+    console.log(player.attributes.track.value)
+    if (player.attributes.track.value < listmusicreal.length - 1) {
+        playerdiv.innerHTML = "<audio id='player' controls track='" + (player.attributes.track.value++) + "'><source src='music/" + listmusicreal[player.attributes.track.value++] + "' /></audio>"
+        player = document.getElementById("player");
+        player.onended = endfunc;
+        console.log(player.attributes.track.value++)
 
-		for (let i = 0; i < btn_music.length; i++) {
-			btn_music[i].className="song"
-		}
+        for (let i = 0; i < btn_music.length; i++) {
+            btn_music[i].className = "song"
+        }
 
-		console.log(player.attributes.track.value)
-		btn_music[player.attributes.track.value].className="song play"
-	}
-	else{
-		console.log("Fin de la liste")
-	}
+        console.log(player.attributes.track.value)
+        btn_music[player.attributes.track.value].className = "song play"
+        document.querySelector('.jukebox__record-image').src = document.querySelector('.play img').src;
+    } else {
+        console.log("Fin de la liste")
+    }
 }
 
-function getList(){
-	btn_music=document.getElementsByClassName("song");
-	console.log(btn_music)
-	console.log(btn_music[0])
-	listmusicreal=[]
+function getList() {
+    btn_music = document.getElementsByClassName("song");
+    console.log(btn_music)
+    console.log(btn_music[0])
+    listmusicreal = []
 
-	for (var i = 0; i < btn_music.length; i++) { //peut être parcourir liste autre sens
-		listmusicreal.push(btn_music[i].attributes.value.value)
-	}
-	console.log(listmusicreal)
+    for (var i = 0; i < btn_music.length; i++) { //peut être parcourir liste autre sens
+        listmusicreal.push(btn_music[i].attributes.value.value)
+    }
+    console.log(listmusicreal)
 
-	playerdiv.innerHTML="<audio id='player' controls autoplay track='0'><source src='music/"+listmusicreal[0]+"' /></audio>";
-	player=document.getElementById("player");
-	player.onended=endfunc;
+    playerdiv.innerHTML = "<audio id='player' controls autoplay track='0'><source src='music/" + listmusicreal[0] + "' /></audio>";
+    player = document.getElementById("player");
+    player.onended = endfunc;
 
-	btn_music[0].className="song play"
+    btn_music[0].className = "song play"
+    document.querySelector('.jukebox__record-image').src = document.querySelector('.play img').src;
 
-	addEventBtn()
+    addEventBtn()
 }
 
-function addEventBtn(){
-	btn_music=document.getElementsByClassName("song");
-	console.log(btn_music)
-	for (let i = 0; i < btn_music.length; i++) {
-		console.log(listmusicreal[i])
-		btn_music[i].addEventListener("click",(e)=>{
-			console.log(e)
-			playerdiv.innerHTML="<audio id='player' controls autoplay track='"+i+"'><source src='music/"+listmusicreal[i]+"' /></audio>";
-			player=document.getElementById("player");
-			player.onended=endfunc;
-			console.log(player.attributes.track.value)
+function addEventBtn() {
+    btn_music = document.getElementsByClassName("song");
+    console.log(btn_music)
 
-			
-			for (let i = 0; i < btn_music.length; i++) {
-				btn_music[i].className="song"
-			}
+    for (let i = 0; i < btn_music.length; i++) {
+        console.log(listmusicreal[i])
+        btn_music[i].addEventListener("click", (e) => {
 
-			console.log(btn_music[i])
-			btn_music[i].className="song play"
-			})
-	};
+            console.log(e)
+            playerdiv.innerHTML = "<audio id='player' controls autoplay track='" + i + "'><source src='music/" + listmusicreal[i] + "' /></audio>";
+            player = document.getElementById("player");
+            player.onended = endfunc;
+            console.log(player.attributes.track.value)
+
+
+            for (let i = 0; i < btn_music.length; i++) {
+                btn_music[i].className = "song"
+            }
+
+            console.log(btn_music[i])
+            console.log('ici');
+            btn_music[i].className = "song play"
+            document.querySelector('.jukebox__record-image').src = document.querySelector('.play img').src;
+        })
+    };
 }
