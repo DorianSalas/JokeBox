305e3f1572852efb9cadbd073c9591cd76154aa5
SalasDorian <salasdorian@live.fr>
2019-08-29 12:16:10 +0200

    prepare addSong

--- js/listSong.js
+++ js/listSong.js
@@ -21,16 +21,28 @@ getSongs('json/listsong.json').then(data => {
 
 document.querySelector('#divAddSong img').addEventListener('click', () => { addSong(); });
 
-function addSong() {
-    Swal.fire({
+async function addSong() {
+    let addSongForm = await Swal.fire({
         titleText: 'Add song',
         html: '<div id="addSongInputGroup"><label for="titleSongInput">Title :</label><input type="text" id="titleSongInput"></input><label for="tagSongInput">Tag :</label><input type="text" id="tagSongInput"></input><label for="imgSong">Choisir une image :</label><input type="file" id="imgSong"><label for="fileSong">Song :</label><input type="file" id="fileSong"></div>',
         focusConfirm: false,
         preConfirm: () => {
+
             return [
-                document.getElementById('logUserInput').value,
-                document.getElementById('logPassInput').value
+                document.getElementById('titleSongInput').value,
+                document.getElementById('tagSongInput').value,
+                document.getElementById('imgSong').value,
+                document.getElementById('fileSong').value
             ]
         }
     });
+    console.log('0');
+    console.log(addSongForm.value[0]);
+    console.log('1');
+    console.log(addSongForm.value[1]);
+    console.log('2');
+    console.log(addSongForm.value);
+    console.log('3');
+    console.log(addSongForm);
+
 }
