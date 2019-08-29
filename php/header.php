dc71ce9d7fa47435885ff7ba6b6ea10b359fc814
Thomas Maurer <ixolidiazerty@gmail.com>
2019-08-29 12:09:20 +0200

    integrated needLogin & needLogout

--- php/header.php
+++ php/header.php
@@ -1,18 +1,16 @@
 <!-- bla -->
 <header>
-
     <div id='divCo'>
-        <p id="connexionText">Log In</p>
+        <p class="needLogout" id="connexionText">Log In</p>
         <div class="arrow-left"></div>
     </div>
 
     <div id='divTitle'>
         <h1 id="mainTitle">JokeBox</h1>
     </div>
-
     <div id='divDeco'>
         <div class="arrow-right"></div>
-        <p>Log Out</p>
+        <p class="needLogin">Log Out</p>
     </div>
 
 </header>
